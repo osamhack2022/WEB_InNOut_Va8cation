0b539d9532e67b9475dd9152bbbb3f27c6b26072
betadoggy <mgs4786@gmail.com>
2022-09-26 00:08:41 +0900

    Fix top navigation bar

--- front-end/src/components/HelloWorld.vue
+++ front-end/src/components/HelloWorld.vue
@@ -4,17 +4,20 @@
       <div>
 
         <v-app-bar color="blue" dense dark>
+          <header class="Header js-details-container Details px-3 px-md-4 px-lg-5 flex-wrap flex-md-nowrap">
+            <div class="Header-item mt-n1 mb-n1  d-none d-md-flex">
+              <v-img src="@/assets/images/INNOUT_logo.svg" alt="In&Out Logo" contain height="32" width="32"></v-img>
+            </div>
 
-          <v-container>
-              <div class="d-flex align-center">
-                <v-img src="@/assets/images/INNOUT_logo.svg" alt="In&Out Logo" contain height="32" width="32"></v-img>
-                <span style="color:white; font-family:AppleSDGothicNeoEB">공지사항</span>
-                <span style="color:white; font-family:AppleSDGothicNeoEB">용사출타현황</span>
-                <span style="color:white; font-family:AppleSDGothicNeoEB">용사출타변경</span>
-                <v-spacer></v-spacer>
-                <span style="color:white; font-family:AppleSDGothicNeoEB">로그아웃</span>
-              </div>
-          </v-container>
+
+            <div class="Header-item">
+              <a class="Header-link mt-md-n3 mb-md-n3 py-2 py-md-3 mr-0 mr-md-3 border-top border-md-top-0 border-white-fade">공지사항</a>
+              <a class="Header-link mt-md-n3 mb-md-n3 py-2 py-md-3 mr-0 mr-md-3 border-top border-md-top-0 border-white-fade">용사출타현황</a>
+              <a class="Header-link mt-md-n3 mb-md-n3 py-2 py-md-3 mr-0 mr-md-3 border-top border-md-top-0 border-white-fade">용사출타수정</a>
+            </div>
+
+            <span style="color:white; font-family:AppleSDGothicNeoEB">로그아웃</span>
+          </header>
 
         </v-app-bar>
       </div>
@@ -25,8 +28,35 @@
 <style>
 @font-face {
   font-family: "AppleSDGothicNeoEB";
-  src: url('../assets/fonts/AppleSDGothicNeoEB.ttf') format('truetype');
+  src: url('@/assets/fonts/AppleSDGothicNeoEB.ttf') format('truetype');
   font-weight: 400;
+}
+
+.Header {
+    z-index: 32;
+    display: flex;
+    padding: 16px;
+    font-size: 14px;
+    line-height: 1.5;
+    color: var(--color-header-text);
+    background-color: var(--color-header-bg);
+    align-items: center;
+    flex-wrap: nowrap;
+}
+
+.Header-item {
+    display: flex;
+    margin-right: 16px;
+    align-self: stretch;
+    align-items: center;
+    flex-wrap: nowrap;
+}
+
+.Header-link {
+    font-weight: 400;
+    font-family: AppleSDGothicNeoEB;
+    color: white;
+    white-space: nowrap;
 }
 </style>
 
