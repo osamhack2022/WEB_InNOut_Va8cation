5096ccda60f5afb9b1cb180222b68c217a67dddb
이동협 <81519841+labyrinth03@users.noreply.github.com>
2022-10-19 13:39:28 +0000

    On branch main  Your branch is up to date with 'origin/main'.  Changes to be committed: 	modified:   front-end/src/App.vue 	modified:   front-end/src/router/router.js

--- front-end/src/router/router.js
+++ front-end/src/router/router.js
@@ -1,7 +1,7 @@
 import { createWebHistory, createRouter } from "vue-router";
 import MyLogin from "@/components/MyLogin"
 import HomePage from "@/components/HomePage"
-import InputPoint from "@/components/InputPoint"
+import OutStatus from "@/components/OutStatus"
 import PasswordReset from "@/components/PasswordReset"
 import JoinPage from "@/components/JoinPage"
 
@@ -10,7 +10,7 @@ const router = createRouter({
     routes : [ // path별 component를 추가한다.
         { path : "/", name : "HomePage", component : HomePage },
         { path : "/login", name : "login", component : MyLogin },
-        { path : "/inputpoint", name : "inputpoint", component : InputPoint },
+        { path : "/outstatus", name : "outstatus", component : OutStatus },
         { path : "/passwordreset", name : "passwordreset", component : PasswordReset },
         { path : "/join", name : "join", component : JoinPage },
     ]
