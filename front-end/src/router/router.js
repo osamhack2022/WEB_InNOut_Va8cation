c18f56fdd582ecd8f258970edef0b758997f53ef
이동협 <81519841+labyrinth03@users.noreply.github.com>
2022-10-23 13:49:30 +0000

    navigation-guard-added

--- front-end/src/router/router.js
+++ front-end/src/router/router.js
@@ -6,16 +6,39 @@ import InputVacation from "@/components/pages/InputVacation"
 import PasswordReset from "@/components/pages/PasswordReset"
 import JoinPage from "@/components/pages/JoinPage"
 import JoinInfo from "@/components/pages/JoinInfo"
-
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
 
 
 const router = createRouter({
     history : createWebHistory(),
     routes : [ // path별 component를 추가한다.
-    { path : "/home", name : "HomePage", component : HomePage },
+    { path : "/home", name : "HomePage", component : HomePage, 
+    beforeEnter: function(to, from, next){
+        var user = firebase.auth().currentUser;
+        if(user != ''){
+          return next()
+        }
+        next('/')
+    } },
     { path : "/", name : "login", component : MyLogin },
-    { path : "/inputpoint", name : "inputpoint", component : InputPoint },
-    { path : "/inputvacation", name : "inputvacation", component : InputVacation },
+    { path : "/inputpoint", name : "inputpoint", component : InputPoint, 
+    beforeEnter: function(to, from, next){
+        var user = firebase.auth().currentUser;
+        if(user != ''){
+          return next()
+        }
+        next('/')
+    }  },
+    { path : "/inputvacation", name : "inputvacation", component : InputVacation, 
+    beforeEnter: function(to, from, next){
+        var user = firebase.auth().currentUser;
+        if(user != ''){
+          return next()
+        }
+        next('/')
+    }  },
     { path : "/passwordreset", name : "passwordreset", component : PasswordReset },
     { path : "/join", name : "join", component : JoinPage },
     { path : "/joininfo", name : "joininfo", component : JoinInfo },
