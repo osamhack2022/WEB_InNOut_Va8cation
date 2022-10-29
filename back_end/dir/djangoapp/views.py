714d624d504ec9a40f23a0b1eccb22a122794f2f
이동협 <81519841+labyrinth03@users.noreply.github.com>
2022-10-29 13:51:44 +0000

    back_end_deleted

--- back_end/dir/djangoapp/views.py
+++
@@ -1,5 +0,0 @@
-from pipes import Template
-from django.views.generic import TemplateView
-
-class HomeView(TemplateView):
-    template_name = 'home.html'
--- back_end/dir/login/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from login.views import LoginLV, LoginDV
-
-app_name = 'login'
-urlpatterns = [
-    path('', LoginLV.as_view(), name='index'),
-    path('<int:pk>/', LoginDV.as_view(), name='detail'),
-]
-
